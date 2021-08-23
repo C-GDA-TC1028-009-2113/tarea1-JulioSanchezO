acb6d4534d58cc7c127ad529d34fef17be859da1
A01641178 <a01641178@itesm.mx>
2021-08-23 03:17:25 +0000

    Tarea 1: Programas que realizan cálculos

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+    pesoIn = float(input('Dame el peso inicial: '))
+    pesoFn = float(input('Dame el peso final: '))
+    mesesMet = int(input('Dame la cantidad de meses: '))
+    pesoxmes = (pesoIn-pesoFn) / mesesMet
+    print('Lo que debes bajar por mes es: ' + str(pesoxmes))
+
 
 
 
--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+    juegosNu = int(input('Dame la cantidad de juegos nuevos: '))
+    juegosUs = int(input('Dame la cantidad de juegos usados: '))
+    totalComp = (juegosNu*1000) + (juegosUs*350)
+    print('El total de la compra es: ' + str(totalComp))
 
 
 
